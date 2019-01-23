eb1774d86a606742c62447b5366ddc96cc5ce4e1
XiaoyongXu <xxy1994212@gmail.com>
2019-01-23 21:40:08 +0000

    finish the assignment of sending text to server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake usersData taken from tweets.json
+
+
+
+
+$(()=>{
+  function loadTweets(){
+    $.ajax({
+      method: "get",
+      url:"/tweets",
+    }).done(function(res){
+      renderTweets(res);
+    });
+  }
+
+  function renderTweets(tweets) {
+    for(tweet of tweets){
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
+  }
+
+  $('#new').submit(function(){
+    event.preventDefault();
+    const serialized = $(this).serialize();
+    console.log(serialized);
+    $.ajax({
+      method: "POST",
+      url:"/tweets",
+      data: serialized,
+
+    }).done(function(){
+      console.log(this.data);
+    })
+  });
+  
+  function createTweetElement(User){
+    let postTime = moment(User.created_at).fromNow();
+    let logo = User.user.avatars.small;
+    let $tweetElem = $("<article>").addClass("tweet").html(
+    
+      `
+      <header>
+        <img src=${logo}>
+        <name>${User.user.name}</name>
+        <handel>${User.user.handle}</handel>
+      </header>
+      <hr>
+      <h4>
+        ${User.content.text}
+      </h4>
+      <hr>
+      <h5>
+        ${postTime}
+      </h5>
+      `
+    )
+    return $tweetElem;
+  }
+
+  loadTweets();
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,22 @@
-$(document).ready(function(){
+$(()=>{
 
   const maxChar = 140;
 
   $("textarea").keyup(function(e){
     let txtChar = $(this).val().length;
-  //  if(txtChar <= maxChar){
+   if(txtChar <= maxChar){
     let counter = maxChar - txtChar;
     if (counter < 0){
       $("#counter").text(counter).css('color','red');
     }else{
       $("#counter").text(counter);
     }
-  //  }
+   }else{
+     alert("too many words");
+   }
   });
- 
- 
+  
+  
   // $("textarea").keydown(function(e){
   //  let keycode = e.keyCode;
   //  let txtChar = $(this).val().length;
