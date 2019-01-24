8fb4ce765b05201305403da7b1ad03cbd0b97193
XiaoyongXu <xxy1994212@gmail.com>
2019-01-24 21:06:37 +0000

    connect and use mongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,18 +9,27 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      
+      db.collection("tweets").insertOne(newTweet,(err, result) => {
+        if(err){
+          callback(err);
+        } else {
+          callback(null, true);
+        }
+      });  
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets.sort(sortNewestFirst));
       });
+      // callback(null, db.collection("tweets").sort(sortNewestFirst));
+      
     }
 
   };
